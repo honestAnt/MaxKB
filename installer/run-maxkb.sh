e1e0158253394992c9d9ab053920cc07e09c0281
fengbao.wang <wangfengbaowfb@gmail.com>
2024-06-01 15:55:01 +0800

    feature: 调整镜像配置

--- installer/run-maxkb.sh
+++ installer/run-maxkb.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # Start postgresql
-docker-entrypoint.sh postgres &
-sleep 10
+#docker-entrypoint.sh postgres &
+#sleep 10
 # Wait postgresql
 until pg_isready --host=127.0.0.1; do sleep 1 && echo "waiting for postgres"; done
 
 # Start MaxKB
+npm run dev
 python /opt/maxkb/app/main.py start
+
