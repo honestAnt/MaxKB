7d2e2f0098498e14ed19698a10187a63900eec73
fengbao.wang <wangfengbaowfb@gmail.com>
2024-06-24 19:36:58 +0800

    feat：调整认证地址6

--- ui/src/utils/login.ts
+++ ui/src/utils/login.ts
@@ -1,5 +1,5 @@
 export function login() {
   // const login_url = `${import.meta.env.VITE_KEYCLOAK_URL}/realms/${import.meta.env.VITE_KEYCLOAK_REALM}/protocol/openid-connect/auth?client_id=${import.meta.env.VITE_KEYCLOAK_CLIENT}&redirect_uri=${window.location.href}?response_mode=query&response_type=token&scope=openid`
-  const login_url = `${import.meta.env.VITE_KEYCLOAK_URL}/login?client_id=${import.meta.env.VITE_KEYCLOAK_CLIENT}&redirect_uri=${window.location.href}?response_mode=query&response_type=token&scope=openid`
+  const login_url = `${import.meta.env.VITE_KEYCLOAK_URL}/login?client_id=${import.meta.env.VITE_KEYCLOAK_CLIENT}&redirect_uri=${window.location.href}&response_mode=query&response_type=token&scope=openid`
   window.open(login_url, '_self')
 }
